7178e01bed151e7575ae8993ea0dba82069a7499
csharpyth0n <121863991+csharpython@users.noreply.github.com>
2023-02-11 16:38:48 +0900

    add javascript.js

---
+++ ABC001-A/ABC001A/javascript.js
@@ -0,0 +1,6 @@
+// atcoder.jp/contests/abc001/submissions/38772920
+function main(input) {
+  const a = input.split("\n"); 
+  console.log(Number(a[0])-Number(a[1]));
+}
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
